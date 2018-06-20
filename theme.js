4fcac3984896c1f57d34c1377dc5885303e61349
ahmedu007 <umair170@hotmail.com>
2018-06-20 19:44:28 +0100

    adding theme for the app

---
+++ theme.js
@@ -0,0 +1,14 @@
+import { COLOR } from "react-native-material-ui";
+
+const uiTheme = {
+  palette: {
+    primaryColor: COLOR.green500
+  },
+  toolbar: {
+    container: {
+      height: 50
+    }
+  }
+};
+
+export default uiTheme;
