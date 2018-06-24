d3245550aacf77c443357df6dd470ba005604373
ahmedu007 <umair170@hotmail.com>
2018-06-24 17:48:28 +0100

    add result screen

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { createStackNavigator } from "react-navigation";
 
 import Options from "./Screens/Options";
 import Home from "./Screens/Homepage";
+import Result from "./Screens/Result";
 
 export default (Navigator = createStackNavigator(
   {
     screen1: { screen: Home },
-    screen2: { screen: Options }
+    screen2: { screen: Options },
+    screen3: { screen: Result }
   },
   {
     headerMode: "none"
